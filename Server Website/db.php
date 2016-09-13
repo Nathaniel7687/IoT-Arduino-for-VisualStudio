1ba1cd0acd05b5bf2d0dba18a4730ba2c32f5a41
Veronica <hj.veronica.shim@gmail.com>
2016-09-13 19:13:32 +0900

    Changed the whole UI

--- Server Website/db.php
+++ Server Website/db.php
@@ -1,5 +1,6 @@
 <?php
-$host = "localhost"; // 자신의 mysql
+// $host = "165.132.121.124"; // 자신의 mysql
+$host = "127.0.0.1";
 $DB_name = "master"; // 데이터베이스 이름
 $user = "root"; // 기본 사용자.
 $password = "0000"; // apm 기본 암호
@@ -16,9 +17,9 @@ $sensor_field_name = array(
 	'가스'
 ); //센서 필드명 집합
 $actuator_field_name = array(
-	'A_Fan',
-	'A_Servo',
-	'A_Buzzer'
+	'Fan',
+	'Servo',
+	'Buzzer'
 ); //액추에이터 필드명 집합
 
 $conn = mysqli_connect($host, $user, $password, $DB_name);
@@ -27,35 +28,33 @@ if (!$conn) { die("DB Connection Error: " .mysqli_error($conn)); }
 
 // else
 if($_POST['table_name'] == 'sensorlist'){
-	$sql = "select S_Ultrasonic,S_IR,S_Humidity,S_Temperature,S_Heatindex,S_Light,S_Gas from sensorlist";
+	$sql = "select IP,S_Ultrasonic,S_IR,S_Humidity,S_Temperature,S_Heatindex,S_Light,S_Gas from SensorList";
 }
 else{
-	$sql = "select A_Fan,A_Servo,A_Buzzer from actuatorlist";
+	$sql = "select IP,A_Fan,A_Servo,A_Buzzer from ActuatorList";
 }
 
 $result = mysqli_query($conn, $sql) or die("Error in Selecting " . mysqli_error($conn));
 
 while ($row = mysqli_fetch_array($result)) {
+	echo "<li class='ui-widget-content ui-corner-tr ui-state-default'>";
+	echo "<h5 class='ui-widget-header'>IP: " . $row['IP'] . "</h5>";
+	echo "<table class='table table-hover table-condensed table-bordered'><tbody>";
 	if($_POST['table_name'] == 'sensorlist'){
-		for ($i = 0; $i < $sensor_field_cnt; $i++) {	//sensor part
+		for ($i = 1; $i <= $sensor_field_cnt; $i++) {	//sensor part
 			if ($row[$i] != NULL) {
-				echo "<li class='ui-widget-content ui-corner-tr'>";
-				echo "<h5 class='ui-widget-header'>" . $sensor_field_name[$i] . " 센서</h5>";
-				echo "<a href='images/high_tatras.jpg' title='View larger image' class='ui-icon ui-icon-zoomin'> View larger</a>";
-				echo "<a href='link/to/trash/script/when/we/have/js/off' title='Delete this image' class='ui-icon ui-icon-trash'>Delete image</a></li>";
+				echo "<tr><td>" . $sensor_field_name[$i-1] . "</td><td>" . $row[$i] . "</td></tr>";
 			}	//if
 		}	//for
 	}	//if
 	else{	//table_name == actuatorlist
-		for($j = 0; $j < $actuator_field_cnt; $j++){	//actuator part
+		for($j = 1; $j <= $actuator_field_cnt; $j++){	//actuator part
 			if($row[$j] != NULL){
-				echo "<li class='ui-widget-content ui-corner-tr'>";
-				echo "<h5 class='ui-widget-header'>" . $actuator_field_name[$j] . "</h5>";
-				echo "<a href='images/high_tatras.jpg' title='View larger image' class='ui-icon ui-icon-zoomin'> View larger</a>";
-				echo "<a href='link/to/trash/script/when/we/have/js/off' title='Delete this image' class='ui-icon ui-icon-trash'>Delete image</a></li>";
+				echo "<tr><td>" . $actuator_field_name[$j-1] . "</td><td>" . $row[$j] . "</td></tr>";
 			}	//if
 		}	//for
 	}
+	echo "</tbody></table></li>";
 }	//while
 
 mysqli_close($conn);
